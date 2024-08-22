9fb11f8949211a8f65718671e997ddc09924cac7
Glenn Crownover <glenn@bluejava.com>
2024-08-23 02:37:34 +0900

    make some suggestions for day01 part1/part2

--- day01/day01_part1.js
+++ day01/day01_part1.js
@@ -1,18 +1,31 @@
-"use strict";
+// Common convention to place require() statements at the top of a file. Also ESM is now supported
+// and is preferred (at least by me!) - You can then omit "use strict" as ESM is always strict mode.
+// Also, I try to stick with the promise returning async versions - and only import the functions I need.
+import { readFile } from 'fs/promises'
 
-// const filename = '/home/jay/source/aoc2023/day01/sample.txt';
-const filename = '/home/jay/source/aoc2023/day01/input.txt';
+// const filename = '/home/jay/source/aoc2023/day01/input.txt';
+// const filename = './input1.txt'; // Answer: 242
+const filename = './input2.txt'; // Answer: 55447
 
-const fs = require('fs');
-const lines = fs.readFileSync(filename, 'utf-8').split(/\r?\n/).filter(l => l.length > 0);
-// console.log(lines);
+// I like to extract functions like these, as they can be reused in multiple places
+// and even when not, it helps self-document the code.
+const identity = x => x; // useful in filter clauses to remove falsy values
+const linebreak = /\r?\n/;  // handles both \r\n and \n
+
+const lines = (await readFile(filename, 'utf-8')) // use of await here behaves just like a synchronous version (or at least looks like it)
+    .split(linebreak) // I like to line break chained functions so dots line up
+    .filter(identity); // as its easier to follow - like a recipe :-)
+
+// Pulling out the regex definition out of the reduce loop is more efficient (maybe... honestly the Node JIT can
+// probably figure this out). Also naming it descriptively self documents the code.
+const singleDigit = /(\d)/;
 
 // use reduce() as an accumulator
-const sum = lines.reduce((running_total, line) => {
-    const regex = /(\d)/;
-    let calibration_value = parseInt(regex.exec(line)[1]) * 10 + 
-        parseInt(regex.exec(line.split('').reverse().join(''))[1]);
-    return running_total + calibration_value;
-}, 0);
+// Note that by removing the calibration_value temporary variable, we can remove both the {} and the return statement
+// hopefully reducing cognitive load. Of course, if we feel the temporary variable helps self-document the code it
+// could be worth leaving in. So this is a nitpick - then again, all my suggestions here are nitpicks!
+const sum = lines.reduce((running_total, line) =>
+    running_total + parseInt(singleDigit.exec(line)[1]) * 10 +
+        parseInt(singleDigit.exec(line.split('').reverse().join(''))[1]), 0);
 
 console.log(sum);
--- day01/day01_part2.js
+++ day01/day01_part2.js
@@ -1,26 +1,33 @@
-"use strict";
+import { readFile } from 'fs/promises'
 
 // const filename = '/home/jay/source/aoc2023/day01/sample2.txt';
-const filename = '/home/jay/source/aoc2023/day01/input.txt';
+// const filename = '/home/jay/source/aoc2023/day01/input.txt';
+const filename = './input2.txt'; // Answer: 54706
 
-const fs = require('fs');
-const lines = fs.readFileSync(filename, 'utf-8').split(/\r?\n/).filter(l => l.length > 0);
+const identity = x => x; // useful in filter clauses to remove falsy values
+const linebreak = /\r?\n/;  // handles both \r\n and \n
+
+const lines = (await readFile(filename, 'utf-8')) // use of await here behaves just like a synchronous version (or at least looks like it)
+    .split(linebreak) // I like to line break chained functions so dots line up
+    .filter(identity); // as its easier to follow - like a recipe :-)
 // console.log(lines);
 
-const convert_name_to_number = {
+const convert_name_to_number = { // This doesn't support 0 (or 'zero') but I guess that's okay...
     '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
     'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9,
 };
 
-let sum = lines.reduce((running_total, line) => {
-    const first_regex = /(\d|one|two|three|four|five|six|seven|eight|nine)/;
-    let calibration_value = 10 * convert_name_to_number[first_regex.exec(line)[1]]; 
+const numberTextRE = /(\d|one|two|three|four|five|six|seven|eight|nine)/;
+// this is a positive lookahead regex - enjoy! Native JavaScript does not support splitting regex across lines... (Glenn: Isn't multiline option available? https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/multiline - though not sure its useful here..)
+const last_regex = /(?=\d|one|two|three|four|five|six|seven|eight|nine).*(\d|one|two|three|four|five|six|seven|eight|nine).*$/;
 
-    // this is a positive lookahead regex - enjoy! Native JavaScript does not support splitting regex across lines...
-    const last_regex = /(?=\d|one|two|three|four|five|six|seven|eight|nine).*(\d|one|two|three|four|five|six|seven|eight|nine).*$/;
-    calibration_value += convert_name_to_number[last_regex.exec(line)[1]];
-
-    return running_total + calibration_value;
-}, 0);
+// Again here I pulled the regex out of the loop. This may not really improve performance, but years of doing this in places
+// where it does matter makes it look "wrong" to me. Also the removal of temporary variables is a style question - I lean
+// towards less temporary variables unless it really helps with readability.
+let sum = lines.reduce((running_total, line) =>
+    running_total +
+        10 * convert_name_to_number[numberTextRE.exec(line)[1]] +
+        convert_name_to_number[last_regex.exec(line)[1]]
+, 0);
 
 console.log(sum);
